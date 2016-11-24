01a053dbca0574900a64a4075bb84f5ae16aee54
Kelly Selden <kellyselden@gmail.com>
2016-11-23 21:37:55 -0800

    add lookup unit tests

--- tests/unit/instance-initializers/router-service-test.js
+++ tests/unit/instance-initializers/router-service-test.js
@@ -1,35 +1,63 @@
 import { initialize } from 'dummy/instance-initializers/router-service';
 import { module, test } from 'qunit';
+import { copy } from 'ember-metal/utils';
 import sinon from 'sinon';
 
-module('Unit | Instance Initializer | router service');
+const router = { testProp: 'test val' };
+
+let lookup;
+let register;
+let ember1Application;
+let ember2AppInstance;
+
+module('Unit | Instance Initializer | router service', {
+  beforeEach() {
+    lookup = sinon.stub().returns(copy(router));
+    register = sinon.spy();
+
+    ember1Application = {
+      container: {
+        lookup,
+      },
+      application: {
+        register
+      }
+    };
+    ember2AppInstance = {
+      lookup,
+      register
+    };
+  }
+});
+
+test('ensure lookup is called for 1.13', function(assert) {
+  initialize(ember1Application);
+
+  assert.deepEqual(lookup.args, [['router:main']]);
+});
+
+test('ensure lookup is called for 2.x', function(assert) {
+  initialize(ember2AppInstance);
+
+  assert.deepEqual(lookup.args, [['router:main']]);
+});
 
 test('ensure register is called for 1.13', function(assert) {
-  let register = sinon.spy();
+  initialize(ember1Application);
 
-  let appInstance = {
-    container: {
-      lookup: sinon.stub().returns({}),
-    },
-    application: {
-      register
-    }
-  };
-
-  initialize(appInstance);
-
-  assert.ok(register.calledOnce);
+  assert.deepEqual(register.args, [[
+    'service:router',
+    router,
+    { singleton: true, instantiate: false }
+  ]]);
 });
 
 test('ensure register is called for 2.x', function(assert) {
-  let register = sinon.spy();
+  initialize(ember2AppInstance);
 
-  let appInstance = {
-    lookup: sinon.stub().returns({}),
-    register
-  };
-
-  initialize(appInstance);
-
-  assert.ok(register.calledOnce);
+  assert.deepEqual(register.args, [[
+    'service:router',
+    router,
+    { singleton: true, instantiate: false }
+  ]]);
 });

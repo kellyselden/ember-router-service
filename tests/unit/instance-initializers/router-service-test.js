878f32d567aa90eb4eb6667109cfbd8ca378b20b
Kelly Selden <kellyselden@gmail.com>
2016-11-23 21:41:05 -0800

    split tests into unit and integration

--- tests/unit/instance-initializers/router-service-test.js
+++ tests/unit/instance-initializers/router-service-test.js
@@ -1,51 +1,8 @@
-import Ember from 'ember';
 import { initialize } from 'dummy/instance-initializers/router-service';
 import { module, test } from 'qunit';
-import destroyApp from '../../helpers/destroy-app';
 import sinon from 'sinon';
 
-module('Unit | Instance Initializer | router service', {
-  beforeEach: function() {
-    Ember.run(() => {
-      this.application = Ember.Application.create();
-      this.appInstance = this.application.buildInstance();
-    });
-  },
-  afterEach: function() {
-    Ember.run(this.appInstance, 'destroy');
-    destroyApp(this.application);
-  }
-});
-
-test('can lookup service', function(assert) {
-  initialize(this.appInstance);
-
-  let lookupSource;
-
-  if (this.appInstance.lookup) {
-    lookupSource = this.appInstance;
-  } else {
-    // ember 1.13 support
-    lookupSource = this.appInstance.container;
-  }
-
-  let router = lookupSource.lookup('service:router');
-
-  assert.ok(router.transitionTo);
-});
-
-test('doesn\'t error when run multiple times (fastboot support)', function(assert) {
-  if (!this.appInstance.hasRegistration) {
-    // skip fastboot check if ember 1.13
-    assert.ok(true);
-    return;
-  }
-
-  initialize(this.appInstance);
-  initialize(this.appInstance);
-
-  assert.ok(this.appInstance.hasRegistration('service:router'));
-});
+module('Unit | Instance Initializer | router service');
 
 test('ensure register is called for 1.13', function(assert) {
   let register = sinon.spy();
